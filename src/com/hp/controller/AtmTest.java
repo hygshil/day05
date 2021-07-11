a4dde92a314b86945973c7f83e9b930fa771f46f
hyg <hygshl1@163.com>
2021-07-11 16:12:35 +0800

    1.atm 完成数据初始化，以及此循环，校验客户密码

---
+++ src/com/hp/controller/AtmTest.java
@@ -0,0 +1,54 @@
+package com.hp.controller;
+
+import com.hp.bean.Customer;
+import com.hp.bean.CustomerData;
+import com.hp.util.TestUtil;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class AtmTest {
+
+    private static String cardid;
+    private static String cardPwd;
+
+    public static void main(String[] args) {
+        //测试客户类的数据
+        CustomerData customerData = CustomerData.getInstance();
+        List<Customer> customerList = customerData.getCustomerList();
+        /*for (Customer customer : customerList) {
+            System.out.println("customer = " + customer);
+        }*/
+        //欢迎界面
+        TestUtil.welcome();
+
+        int i = 0;
+        while(i<6){//当输入超过5次就跳出循环
+            //输入用户名、密码
+            doWritePassword();
+            //其他操作：1.校验角色  2.判断客户账号密码是否正确
+            dochaeckpassword(cardid,cardPwd);
+            i++;
+         }
+    }
+    //校验角色和密码
+    private static void dochaeckpassword(String cardid,String cardPwd) {
+    //先校验角色，用密码长度判断
+        if (cardid.length()==8){
+            //校验密码是否正确
+        }
+    }
+
+    //输入用户名密码类
+    private static void doWritePassword() {
+        System.out.println("请输入卡号");
+        Scanner scanner = new Scanner(System.in);
+        cardid = scanner.nextLine();
+        System.out.println("cardid = " + cardid);
+        System.out.println("请输入密码");
+        cardPwd = scanner.nextLine();
+        System.out.println("cardPwd = " + cardPwd);
+    }
+
+
+}
---
+++ src/com/hp/util/TestUtil.java
@@ -0,0 +1,10 @@
+package com.hp.util;
+
+public class TestUtil {
+    public static void welcome() {
+        System.out.println("****************");
+        System.out.println("先生/女士");
+        System.out.println("欢迎欢迎ATM取款机");
+        System.out.println("****************");
+    }
+}
